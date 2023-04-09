602f636c806c51acb607ed3c8807de57045aa019
zeus <zeus1zeus101@gmail.com>
2023-04-10 01:37:38 +0300

    fix

--- services/github_api.py
+++ services/github_api.py
@@ -5,10 +5,12 @@ import requests
 from core.config import get_settings
 
 HEADERS = {
-         'Authorization': f'Bearer {get_settings().git_hub_token}',
-         'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+    'Authorization': f'Bearer {get_settings().git_hub_token}',
+    'Accept': 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28'
+}
+
+
 def create_repo(name):
     github_api_url = 'https://api.github.com/user/repos'
     data = {
@@ -23,13 +25,15 @@ def create_repo(name):
         print(f"Repository {name} already exists.")
         return 422
     else:
-        print(f"Failed to create repository {name}. Status code: {response.status_code} {response.text} {response.request.body}")
+        print(
+            f"Failed to create repository {name}. Status code: {response.status_code} {response.text} {response.request.body}")
         return None
+
 
 def add_file_action(repo):
     path = '.github/workflows/test_app.yml'
     url = f"https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/contents/{path}"
-    content = base64.b64encode(open(os.getcwd()+'/services/example_action.txt','rb').read()).decode()
+    content = base64.b64encode(open(os.getcwd() + '/services/example_action.txt', 'rb').read()).decode()
     print(content)
     params = {
         "message": "Add test_app.yml",
@@ -37,62 +41,72 @@ def add_file_action(repo):
         "path": path
     }
     response = requests.put(url, headers=HEADERS, json=params)
-    print(response.status_code,response.json())
+    print(response.status_code, response.json())
+
 
 def add_webhook(repo):
     url = f"https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/hooks"
     params = {
-              "name": "web",
-              "active": True,
-              "events": [
-                "workflow_run_completed"
-              ],
-              "config": {
-                "url": "https://jira.adstools.net/applications/finish_action",
-                "content_type": "json",
-                "secret": "your-webhook-secret"
-              }
-            }
+        "name": "web",
+        "active": True,
+        "events": [
+            "workflow_run"
+        ],
+        "config": {
+            "url": "https://jira.adstools.net/applications/finish_action",
+            "content_type": "json",
+            "secret": "your-webhook-secret"
+        }
+    }
     response = requests.post(url, headers=HEADERS, json=params)
-    print(response.status_code,response.text)
+    print(response.status_code, response.text)
 
 
-def send_collaborators(repo,to_user):
+def send_collaborators(repo, to_user):
     url = f'https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/collaborators/{to_user}'
     print(url)
-    data = {"permission":"admin"}
-    response = requests.put(url,headers=HEADERS,json=data)
-    print(response.status_code,response.text)
+    data = {"permission": "admin"}
+    response = requests.put(url, headers=HEADERS, json=data)
+    print(response.status_code, response.text)
 
-def run_action(repo,workflow_id):
-    url = f'https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/actions/runs/{workflow_id}/rerun'
+
+def run_action(repo, workflow_id):
+    url = f'https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/actions/workflows/{workflow_id}/dispatches'
+    data = {
+        "ref": "main",
+        "inputs": {
+            "my_input": "value1"
+        }
+    }
     HEADERS = {
         'Authorization': f'Bearer {get_settings().git_hub_token}',
         'Accept': 'application/vnd.github.v3+json',
         'X-GitHub-Api-Version': '2022-11-28'
     }
-    response = requests.post(url,headers=HEADERS)
-    print(response.status_code,response.text)
+    response = requests.post(url, headers=HEADERS,json=data)
+    print(response.status_code, response.text)
 
-def show_result_action(repo,run_id):
+
+def show_result_action(repo, run_id):
     url = f'https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/actions/runs/{run_id}'
     response_run = requests.get(url, headers=HEADERS).json()
     if response_run.get('conclusion') == 'success':
         headers = {
             'Authorization': f'Bearer {get_settings().git_hub_token}',
-            #'Accept': 'application/vnd.github.v3+json',
-            'accept-encoding':'identify',
+            # 'Accept': 'application/vnd.github.v3+json',
+            'accept-encoding': 'identify',
             'X-GitHub-Api-Version': '2022-11-28'
         }
-        response_log = requests.get(response_run['logs_url'],headers=headers).content
-        with open('111.zip','wb') as writer:
+        response_log = requests.get(response_run['logs_url'], headers=headers).content
+        with open('111.zip', 'wb') as writer:
             writer.write(response_log)
     else:
         print(response_run)
 
+
 def show_workflow(repo):
     url = f'https://api.github.com/repos/{get_settings().git_hub_user}/{repo}/actions/workflows'
-    response = requests.get(url,headers=HEADERS).json()
+    response = requests.get(url, headers=HEADERS).json()
     return response['workflows'][0]
 
-
+add_webhook('AP-86')
