10d0fb4555d09c693cff5a5ea6a14cc2e805000e
zeus <zeus1zeus101@gmail.com>
2023-04-14 08:36:21 +0300

    add gitignore

---
+++ bot/app.py
@@ -0,0 +1,12 @@
+from loader import bot, storage , loop
+
+
+async def on_shutdown(dp):
+    await bot.close()
+    await storage.close()
+
+
+if __name__ == '__main__':
+    from aiogram import executor
+    from handlers import dp
+    executor.start_polling(dp,loop=loop,on_shutdown=on_shutdown)
